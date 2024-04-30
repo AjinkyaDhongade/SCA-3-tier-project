8daea8b5e4354829b19df34018d624556038d048
Ajinkya Dhongade <ajinkya.dhongade@esds.co.in>
2024-04-30 17:42:28 +0530

    added code in vite.config.js

--- sca_vite/vite.config.js
+++ sca_vite/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   server: {
+//     host: '172.16.22.122',
+//     port: 30400,
+//   },
 export default defineConfig({
+  plugins: [react()],
+  hot: true,
+  open: true,
   server: {
-    host: '172.16.22.122',
-    port: 30400,
+    port: 3000,
+    open: false,
+    host: '0.0.0.0',
   },
   plugins: [],
 })
